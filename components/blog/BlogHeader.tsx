84f95d0f017d5ccdac1d7e46bfc91fb3fa7b6532
Quinn Xie <antlossway@gmail.com>
2024-01-03 10:40:35 +0700

    modify BlogHeader, use bg-blend-multiply to achieve the effect that clear text on top of darker overlay background image

--- components/blog/BlogHeader.tsx
+++ components/blog/BlogHeader.tsx
@@ -8,23 +8,23 @@ type Props = {
 }
 const BlogHeader = ({ totalNumOfPost }: Props) => {
   return (
-    <div className="w-full h-[200px] bg-[url('/blog-background-datacenter.jpeg')]  ">
-      {/* overlay shadow */}
-      <div className="w-full h-[200px] bg-dark-700/50 p-8 px-10 grid place-items-center">
-        {/* header with logo and title */}
-        <div className=" mx-auto w-full max-w-5xl p-4 flex justify-between items-center  ">
-          <SiteLogo />
-          <div className="flex items-center gap-8 ">
-            <p className="hidden sm:block text-center text-3xl text-white sm:text-4xl font-bold">
-              <Link href="/blog" className="hover:text-white/80">
-                Articles
-                {totalNumOfPost ? (
-                  <span className="text-lg"> (total {totalNumOfPost})</span>
-                ) : null}
-              </Link>
-            </p>
-            <ThemeSwitch />
-          </div>
+    <div
+      className="w-full h-[200px] bg-left-top object-cover bg-[url('/blog-background-datacenter.jpeg')] bg-dark-700/50 bg-blend-multiply p-8 px-10 grid place-items-center
+       "
+    >
+      {/* header with logo and title */}
+      <div className=" mx-auto w-full max-w-5xl p-4 flex justify-between items-center  ">
+        <SiteLogo />
+        <div className="flex items-center gap-8 ">
+          <p className="hidden sm:block text-center text-3xl text-white sm:text-4xl font-bold">
+            <Link href="/blog" className="hover:text-white/80">
+              Articles
+              {totalNumOfPost ? (
+                <span className="text-lg"> (total {totalNumOfPost})</span>
+              ) : null}
+            </Link>
+          </p>
+          <ThemeSwitch />
         </div>
       </div>
     </div>
