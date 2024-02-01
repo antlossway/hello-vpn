6ee293fc564a075d79179d27da7d710126a67c51
Quinn Xie <antlossway@gmail.com>
2024-02-01 17:38:52 +0700

    add revalidate api for incremental static regeneration

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,26 @@
+//on-demand revalided path
+
+import { NextRequest, NextResponse } from "next/server"
+import { revalidatePath } from "next/cache"
+
+export async function GET(req: NextRequest) {
+  const secret = req.nextUrl.searchParams.get("secret")
+  if (secret !== process.env.MY_SECRET_TOKEN) {
+    return new NextResponse(JSON.stringify({ message: "Invalid Token" }), {
+      status: 401,
+      statusText: "Unauthorized",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  }
+
+  const path = req.nextUrl.searchParams.get("path") || "/blog"
+
+  revalidatePath(path)
+
+  return NextResponse.json({
+    revalidated: true,
+    path,
+  })
+}
