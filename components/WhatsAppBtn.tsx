176fe8b481689c3c213645b0c2ea9f7c3360ba9c
Quinn Xie <antlossway@gmail.com>
2024-02-28 13:39:50 +0700

    add umami event

--- components/WhatsAppBtn.tsx
+++ components/WhatsAppBtn.tsx
@@ -5,7 +5,13 @@ const WhatsAppBtn = () => {
   const number = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER
   const url = `https://wa.me/${number}`
   return (
-    <a aria-label="Chat on WhatsApp" target="_blank" href={url}>
+    <a
+      aria-label="Chat on WhatsApp"
+      target="_blank"
+      href={url}
+      id="contact-whatsapp-chat"
+      data-umami-event="contact-whatsapp-chat"
+    >
       <Image
         alt="Chat on WhatsApp"
         src="/WhatsAppButtonGreenMedium.png"
